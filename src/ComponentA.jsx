818317a4d5c3d191f28c06900b9eb3afe07902ab
Krishna-zz <krishnagunjal022@gmail.com>
2025-06-29 08:48:05 +0530

    feat: initialize React app with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created main application structure with App and ComponentA components.
    - Implemented a stopwatch feature in ComponentA with start, stop, and reset functionality.
    - Added SVG assets for React and Vite logos.
    - Configured Vite with React and Tailwind CSS plugins.
    - Included global styles with Tailwind CSS in index.css.

---
+++ src/ComponentA.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from "react"
+
+
+function ComponentA(){
+
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    let intervalIDRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+        if(isRunning){
+            intervalIDRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current)
+            }, 10);
+        }
+
+        return() => {clearInterval(intervalIDRef.current)}
+    }, [isRunning])
+
+    const start = () => {
+        startTimeRef.current = Date.now() - elapsedTime
+        setIsRunning(true)
+    }
+
+    const stop = () => {
+        setIsRunning(false)
+    }
+
+    const reset = () => {
+        setElapsedTime(0);
+        setIsRunning(false)
+
+    }
+
+    const formatTime = () => {
+
+        let minutes = Math.floor(elapsedTime/ (1000 * 60) % 60)
+        let seconds = Math.floor(elapsedTime/ (1000) % 60)
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10)
+
+        return`${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`
+    }
+
+    const pad = (number) => {
+        return (number < 10? "0": "") + number
+    }
+
+    return(
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-600 via-orange-600 to-pink-600 ">
+    <div className="bg-white rounded-2xl shadow-2xl p-10 w-80 text-center">
+      <h1 className="text-3xl font-bold m-4">⏱ Stopwatch</h1>
+
+      <div className="text-3xl m-9">{formatTime()}</div>
+
+      <div className="flex justify-between">
+        <button
+          onClick={start}
+          className="bg-teal-400 hover:bg-teal-500 px-5 py-2 rounded-xl text-white shadow-xl "
+        >
+          Start
+        </button>
+        
+        <button
+          onClick={reset}
+          className="bg-yellow-400 hover:bg-yellow-500 px-5 py-2 rounded-xl text-white shadow-xl"
+        >
+          Reset
+        </button>
+        <button
+          onClick={stop}
+          className="bg-violet-500 hover:bg-violet-600 px-5 py-2 rounded-xl text-white shadow-xl"
+        >
+          Stop
+        </button>
+      </div>
+    </div>
+  </div>
+    )
+
+}
+
+export default ComponentA
